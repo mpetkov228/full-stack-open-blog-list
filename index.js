b8dc5353dad1874cfef7a851125d3b58d4ea4126
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-25 11:58:58 +0300

    Extract blog model in separate module

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
+const Blog = require('./models/blog');
 
 app.use(cors());
 app.use(express.json());
@@ -36,7 +27,7 @@ app.post('/api/blogs', (request, response) => {
     });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
