5db0cafa9e9995281cc97da2da0a3972c706efbb
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-26 16:25:10 +0300

    Refactor

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const blogsRouter = require('./controllers/blogs')
+
+const app = express();
+
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
