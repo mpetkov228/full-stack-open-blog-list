2e8fe3de7940417f8ac1f8af6a3512e75e89b600
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-26 16:43:24 +0300

    Add a request logger middleware

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 
@@ -23,6 +24,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 
