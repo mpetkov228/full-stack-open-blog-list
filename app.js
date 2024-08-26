c8434362cf9b7e124a5631bb3bbfbed45b7d7f36
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-26 16:35:56 +0300

    Extract db connection from blogs.js to app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs')
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+
+logger.info(`connecting to ${config.MONGODB_URI}`);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB', error.message);
+  });
 
 app.use(cors());
 app.use(express.static('dist'));
--- models/blog.js
+++ models/blog.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose');
-const config = require('../utils/config');
-const logger = require('../utils/logger');
-
-mongoose.set('strictQuery', false);
-
-const url = config.MONGODB_URI;
-mongoose.connect(url)
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch(error => {
-    logger.error('error connecting to MongoDB', error.message);
-  });
 
 const blogSchema = mongoose.Schema({
   title: String,
